b09f3b0e280eedf9cc683a296e61779fa9258c69
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:24:00 +0200

    Delete total_costs_of_electricity_prepayment_per_branch.sql

--- total_costs_of_electricity_prepayment_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningElectricityPrepayment
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Electricity prepayment"
-GROUP BY
-    BranchId;
