2a21467ca3b9bf8921a2670b7fd3a056dbd327e0
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:25:43 +0200

    Delete total_costs_of_minior_repairs_per_branch.sql

--- total_costs_of_minior_repairs_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningMinorRepairs
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Minor repairs"
-GROUP BY
-    BranchId;
