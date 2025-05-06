b9accc7af080afd408eafac680b14ae23e1699b4
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:22:42 +0200

    Delete geocode_address_test.sql

--- geocode_address_test.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION `my-project-scenario2-vlba2.branches.geocode_address`(address STRING)
-RETURNS GEOGRAPHY
-REMOTE
-WITH CONNECTION `projects/my-project-scenario2-vlba2/locations/europe-west3/connections/geocode_function_connection`
-OPTIONS (
-  endpoint = 'https://europe-west3-my-project-scenario2-vlba2.cloudfunctions.net/geocode'  
-);
