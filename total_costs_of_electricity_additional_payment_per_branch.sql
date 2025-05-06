bae297da32f4532136fbd13149b5d22b9eb37826
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:24:10 +0200

    Delete total_costs_of_electricity_additional_payment_per_branch.sql

--- total_costs_of_electricity_additional_payment_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    AVG(CAST(Costs AS FLOAT64)) AS AvgRunningElectricityAdditionalPayment
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Electricity additional payment"
-GROUP BY
-    BranchId;
