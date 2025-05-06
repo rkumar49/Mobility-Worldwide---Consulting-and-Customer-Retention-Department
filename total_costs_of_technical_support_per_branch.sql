a466a46e455dd8e7e4ba552c0873a17ae1f8e736
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:27:00 +0200

    Delete total_costs_of_technical_support_per_branch.sql

--- total_costs_of_technical_support_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SUMRunningTechnicalSupport
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Technical support"
-GROUP BY
-    BranchId;
