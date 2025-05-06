14be4838c4991f132544ea2aa5dcca21cc852350
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:22:23 +0200

    Delete total_costs_of_heating_per_branch.sql

--- total_costs_of_heating_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningHeatingCosts
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Heating costs"
-GROUP BY
-    BranchId;
