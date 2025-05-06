1d1a232a457dfcdacb75f8f09b0512ec15d001b0
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:23:38 +0200

    Delete Training_dataset_3.sql

--- Training_dataset_3.sql
+++
@@ -1,7 +0,0 @@
--- Create a dataset for clustering potential new branch locations
-CREATE OR REPLACE TABLE my-project-scenario2-vlba2.branches.client_locations AS
-SELECT 
-    CAST(SPLIT(REGEXP_REPLACE(DestinationCoordinates, r'[()]', ''), ',')[OFFSET(0)] AS FLOAT64) AS Latitude,
-    CAST(SPLIT(REGEXP_REPLACE(DestinationCoordinates, r'[()]', ''), ',')[OFFSET(1)] AS FLOAT64) AS Longitude
-FROM 
-    my-project-scenario2-vlba2.branches.FinalBusinessTripsSummary
