823f5f698c2fe9f221068109ce6171c959860133
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:28:25 +0200

    Delete total_costs_of_window_replacement_per_branch.sql

--- total_costs_of_window_replacement_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningWindowReplacement
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Window replacement"
-GROUP BY
-    BranchId;
