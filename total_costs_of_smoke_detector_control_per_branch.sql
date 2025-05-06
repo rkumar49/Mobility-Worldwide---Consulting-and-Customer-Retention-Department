9f5531ee8db77764842b1ec20acff55df635e08d
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:28:34 +0200

    Delete total_costs_of_smoke_detector_control_per_branch.sql

--- total_costs_of_smoke_detector_control_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningSmokeDetectorControl
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Smoke detector control"
-GROUP BY
-    BranchId;
