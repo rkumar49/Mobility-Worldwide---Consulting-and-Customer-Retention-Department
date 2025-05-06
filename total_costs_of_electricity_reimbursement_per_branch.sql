08c172f076696baba82ccba062b8077ec2384f9f
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:24:44 +0200

    Delete total_costs_of_electricity_reimbursement_per_branch.sql

--- total_costs_of_electricity_reimbursement_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningElectricityReimbursement
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Electricity reimbursement"
-GROUP BY
-    BranchId;
