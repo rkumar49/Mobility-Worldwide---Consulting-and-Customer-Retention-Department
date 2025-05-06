71e1a95b6da87ebc9e5bafec86fa45419c0c4dbd
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:20:08 +0200

    Delete Query_avg_running_cost_model.sql

--- Query_avg_running_cost_model.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE MODEL branches.avg_running_cost_model
-OPTIONS(model_type='linear_reg', input_label_cols=['AvgRunningCost']) AS
-SELECT BranchId, AVG(SUMRunningCost) as AvgRunningCost, Year
-FROM branches.ToTALCOSTSOFABRANCHPERYEAR 
-Where BranchId in ('B100007', 'B100008', 'B100009')
-GROUP BY BranchId, Year
