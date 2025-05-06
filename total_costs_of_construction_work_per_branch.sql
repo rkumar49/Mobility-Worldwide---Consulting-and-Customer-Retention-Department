af1cb09624dddab6f9e48145aa5fe21f554c1c7c
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:22:07 +0200

    Delete total_costs_of_construction_work_per_branch.sql

--- total_costs_of_construction_work_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    SUM(CAST(Costs AS FLOAT64)) AS SumRunningConstructionWork
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Construction work"
-GROUP BY
-    BranchId;
