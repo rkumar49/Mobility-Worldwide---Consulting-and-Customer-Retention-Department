bd485e40a4706136c3f63073f7503f8ebd9b367e
rkumar49 <Rahulitssameer@gmail.com>
2025-05-06 15:22:00 +0200

    Delete total_costs_of_electrician_per_branch.sql

--- total_costs_of_electrician_per_branch.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    BranchId,
-    AVG(CAST(Costs AS FLOAT64)) AS AvgRunningCostsElectrician,
-FROM
-    `my-project-scenario2-vlba2.branches.MwwCcrBranchRunningCosts_2`
-WHERE
-    ProcessType = "Electrician"
-GROUP BY
-    BranchId;  
